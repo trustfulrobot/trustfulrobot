b3acf9ca22cc622fbb3f2a16d7b98bcdea59f914
Patrick Morrissey <holla@patrickmorrissey.com>
2020-02-16 14:36:45 -0500

    Create gatsby-browser.js file

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,14 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-sass`]
+  plugins: [
+	  {
+	    resolve: `gatsby-plugin-sass`,
+	    options: {
+	      // cssLoaderOptions: {
+	      //   camelCase: false,
+	      // },
+	    },
+	  },
+	]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
 import React from "react"
-import("./src/sass/styles.scss")
 
 export default () => <div>Hello world!</div>
