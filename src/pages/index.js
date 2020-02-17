1c5cfcf347c8217da0cdfeea2d455b4bc824d6d8
Patrick Morrissey <holla@patrickmorrissey.com>
2020-02-17 16:15:38 -0500

    Fix color shift

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,14 +5,14 @@ import "../sass/styles.scss"
 const colorShadows = "#fd1b85";
 const colorHighlights = "#d4efed";
 const colorShadows_R = Color.rgb(colorShadows).red();
-const colorShadows_G = Color.rgb(colorShadows).blue();
-const colorShadows_B = Color.rgb(colorShadows).green();
+const colorShadows_G = Color.rgb(colorShadows).green();
+const colorShadows_B = Color.rgb(colorShadows).blue();
 const colorHighlights_R = Color.rgb(colorHighlights).red();
-const colorHighlights_B = Color.rgb(colorHighlights).blue();
-const colorHighlights_G = Color.rgb(colorHighlights).green();
-const feFuncRValues = colorShadows_R / 256 + " " + colorHighlights_R / 256;
-const feFuncGValues = colorShadows_G / 256 + " " + colorHighlights_G / 256;
-const feFuncBValues = colorShadows_B / 256 + " " + colorHighlights_B / 256;
+const colorHighlights_B = Color.rgb(colorHighlights).green();
+const colorHighlights_G = Color.rgb(colorHighlights).blue();
+const feFuncRValues = colorShadows_R / 255 + " " + colorHighlights_R / 255;
+const feFuncGValues = colorShadows_G / 255 + " " + colorHighlights_G / 255;
+const feFuncBValues = colorShadows_B / 255 + " " + colorHighlights_B / 255;
 
 export default () => <div className="container"><img className="background" src="/images/PatrickBatemanFistbump_background.jpg" alt="Soon Come - Background - Room" srcSet="/images/PatrickBatemanFistbump_background-480x640.jpg 480w, /images/PatrickBatemanFistbump_background-768x1024.jpg 768w, /images/PatrickBatemanFistbump_background-960x1280.jpg 960w, /images/PatrickBatemanFistbump_background-1280x1707.jpg 1280w,	/images/PatrickBatemanFistbump_background-1536x2048.jpg 1536w, /images/PatrickBatemanFistbump_background-1920x2560.jpg 1920w, /images/PatrickBatemanFistbump_background.jpg 2560w" /><div className="sooncome"><span>SOON COME</span></div><img className="foreground" src="/images/PatrickBatemanFistbump_foreground.png" alt="Soon Come - Foreground - Room" srcSet="/images/PatrickBatemanFistbump_foreground-480x640.png 480w, /images/PatrickBatemanFistbump_foreground-768x1024.png 768w, /images/PatrickBatemanFistbump_foreground-960x1280.png 960w, /images/PatrickBatemanFistbump_foreground-1280x1707.png 1280w,	/images/PatrickBatemanFistbump_foreground-1536x2048.png 1536w, /images/PatrickBatemanFistbump_foreground-1920x2560.png 1920w, /images/PatrickBatemanFistbump_foreground.png 2560w" />
   <svg xmlns="http://www.w3.org/2000/svg" className="svg-filters">
