f65123ecd4c112db943b0284c77593525fddb328
Patrick Morrissey <holla@patrickmorrissey.com>
2020-02-17 13:11:32 -0500

    Finalize setting up DOM for now

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,4 @@
 import React from "react"
 import "../sass/styles.scss"
 
-export default () => <div>SOON 
-
-COME</div>
+export default () => <div className="container"><img className="background" src="/images/PatrickBatemanFistbump_background.jpg" alt="Soon Come - Background - Room" srcset="/images/PatrickBatemanFistbump_background-480x640.jpg 480w, /images/PatrickBatemanFistbump_background-768x1024.jpg 768w, /images/PatrickBatemanFistbump_background-960x1280.jpg 960w, /images/PatrickBatemanFistbump_background-1280x1707.jpg 1280w,	/images/PatrickBatemanFistbump_background-1536x2048.jpg 1536w, /images/PatrickBatemanFistbump_background-1920x2560.jpg 1920w, /images/PatrickBatemanFistbump_background.jpg 2560w" /><div className="sooncome">SOON COME</div><img className="foreground" src="/images/PatrickBatemanFistbump_foreground.png" alt="Soon Come - Foreground - Room" srcset="/images/PatrickBatemanFistbump_foreground-480x640.png 480w, /images/PatrickBatemanFistbump_foreground-768x1024.png 768w, /images/PatrickBatemanFistbump_foreground-960x1280.png 960w, /images/PatrickBatemanFistbump_foreground-1280x1707.png 1280w,	/images/PatrickBatemanFistbump_foreground-1536x2048.png 1536w, /images/PatrickBatemanFistbump_foreground-1920x2560.png 1920w, /images/PatrickBatemanFistbump_foreground.png 2560w" /></div>
