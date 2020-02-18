cc2bdb0555743fa3417e4606c7a6c31d04f7c274
Patrick Morrissey <holla@patrickmorrissey.com>
2020-02-18 08:23:40 -0500

    Configure gatsby-plugin-favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,28 @@
 module.exports = {
   /* Your site config here */
   plugins: [
-  `gatsby-plugin-sass`
+  `gatsby-plugin-sass`,
+  resolve: `gatsby-plugin-favicon`,
+  	options: {
+  		logo: './src/favicon.png',
+  		appName: 'Trustful Robot',
+  		appDescription: 'Trustful Robot Webite',
+  		developerName: 'Patrick Morrissey',
+  		developerURL: 'https://trustfulrobot.com',
+  		display: 'browser',
+      lang: 'en-US',
+      background: '#fff',
+      theme_color: '#fff',
+      icons: {
+        android: true,
+        appleIcon: true,
+        appleStartup: true,
+        coast: false,
+        favicons: true,
+        firefox: true,
+        yandex: false,
+        windows: false
+      }
+  	}
   ],
 }
