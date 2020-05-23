680fdaac64052b2e71dc5a13e49ec48516d5bfbd
Patrick Morrissey <holla@patrickmorrissey.com>
2020-05-23 17:39:18 -0400

    Add svg

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,7 @@ module.exports = {
   },
   `gatsby-plugin-sass`,
   `gatsby-plugin-react-helmet`,
+  
   {
 	  resolve: `gatsby-plugin-favicon`,
   	options: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -41,7 +41,25 @@ export default ({ data }) => (
     </Helmet>
     <main>
       <h1>Trustful Robot</h1>
-      <h2>Digital Shamanism</h2>
+      <p>I am a Senior Developer with a history of serving in both front-end and back-end roles that has also, at times, served as a content author and liaison with website clients. As such, I feel that I have a deep sensitivity to the full spectrum of perspectives of those involved in the full life-cycle of a website. I seek to create web experiences that are not only elegant as a user or content author but are a delight to work with as a fellow developer, based around a rock-solid foundational knowledge of semantic HTML, CSS, and Javascript.</p>
+
+      <p>I am a voracious and insatiable learner, constantly searching for increasingly optimal paths and revolutionary insights.</p>
+
+      <h2>Technology</h2>
+      <h3>My current preferred tech stack:</h3>
+      <ul>
+        <li>Static site generation with GatsbyJS and GraphQL</li>
+        <li>Content management with Prismic</li>
+        <li>Hosting with Netlify & AWS</li>
+        <li>Continuous Integration with GitHub</li>
+      </ul>
+
+      <h3>My preferred WordPress tech stack:</h3>
+      <ul>
+        <li>Underscores starter theme</li>
+        <li>WPGulp workflow based on npm and Sass</li>
+        <li>Advanced Custom Fields Pro</li>
+      </ul>
     </main>
   </div>
 )
