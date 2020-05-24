259f6eaf84a4aaf999fb908c0196c993fe3c218b
Patrick Morrissey <holla@patrickmorrissey.com>
2020-05-24 09:59:20 -0400

    Normalize margins on headings

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,30 +40,34 @@ export default ({ data }) => (
     <main>
       <section className="tr_block tr_block__intro">
         <h1><img src={`${svg_logo}`} alt="Trustful Robot | Digital Shamanism" /></h1>
-        <p>I am a Senior Developer with a history of serving in both front-end and back-end roles that has also, at times, served as a content author and liaison with website clients. As such, I feel that I have a <em>deep sensitivity</em> to the entire spectrum of perspectives of those involved in the full life-cycle of a website. I seek to create web experiences that are not only elegant for users and content authors but also a delight with which to work for fellow developers, based around a rock-solid foundational knowledge of semantic HTML, CSS, and Javascript.</p>
+        <div id="introtext">
+          <p>I am a Senior Developer with a history of serving in both front-end and back-end roles that has also, at times, served as a content author and liaison with website clients. As such, I feel that I have a <em>deep sensitivity</em> to the entire spectrum of perspectives of those involved in the full life-cycle of a website. I seek to create web experiences that are not only elegant for users and content authors but also a delight with which to work for fellow developers, based around a rock-solid foundational knowledge of semantic HTML, CSS, and Javascript.</p>
 
-        <p>I am a <strong>voracious and insatiable learner</strong>, constantly searching for increasingly optimal paths and revolutionary insights.</p>
+          <p>I am a <strong>voracious and insatiable learner</strong>, constantly searching for increasingly optimal paths and revolutionary insights.</p>
+        </div>
       </section>
 
       <section className="tr_block tr_block__technology">
         <h2>Technology</h2>
-        <section>
-          <h3>My current preferred tech stack:</h3>
-          <ul>
-            <li>Static site generation with <strong>GatsbyJS</strong> and <strong>GraphQL</strong></li>
-            <li>Content management with <strong>Prismic</strong></li>
-            <li>Hosting with <strong>Netlify</strong> & <strong>AWS</strong></li>
-            <li><strong>Continuous Integration</strong> with <strong>GitHub</strong></li>
-          </ul>
-        </section>
-        <section>
-          <h3>My preferred <strong>WordPress</strong> tech stack:</h3>
-          <ul>
-            <li><strong>Underscores</strong> starter theme</li>
-            <li><strong>WPGulp</strong> workflow based on <strong>npm</strong> and <strong>SASS</strong></li>
-            <li><strong>Advanced Custom Fields Pro</strong></li>
-          </ul>
-        </section>
+        <div id="techstacks">
+          <section>
+            <h3>My current preferred tech stack:</h3>
+            <ul>
+              <li>Static site generation with <strong>GatsbyJS</strong> and <strong>GraphQL</strong></li>
+              <li>Content management with <strong>Prismic</strong></li>
+              <li>Hosting with <strong>Netlify</strong> & <strong>AWS</strong></li>
+              <li><strong>Continuous Integration</strong> with <strong>GitHub</strong></li>
+            </ul>
+          </section>
+          <section>
+            <h3>My preferred <strong>WordPress</strong> tech stack:</h3>
+            <ul>
+              <li><strong>Underscores</strong> starter theme</li>
+              <li><strong>WPGulp</strong> workflow based on <strong>npm</strong> and <strong>SASS</strong></li>
+              <li><strong>Advanced Custom Fields Pro</strong></li>
+            </ul>
+          </section>
+        </div>
       </section>
       <section className="tr_block tr_block__work">
         <h2>Work</h2>
