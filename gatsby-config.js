9c2d0d66c5074fdb6bc175812b0164200d5943c8
Patrick Morrissey <holla@patrickmorrissey.com>
2020-07-20 08:40:44 -0400

    Add gatsby-plugin-preact

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,7 @@ module.exports = {
   `gatsby-plugin-react-helmet`,
   `gatsby-transformer-sharp`,
   `gatsby-plugin-sharp`,
+  `gatsby-plugin-preact`,
   {
     resolve: `gatsby-plugin-favicon`,
     options: {
