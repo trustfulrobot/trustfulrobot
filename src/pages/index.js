3889206cbbeb3f1494470e887ec1f642777fd437
Patrick Morrissey <holla@patrickmorrissey.com>
2020-08-11 11:07:39 -0400

    Initial simple animations

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { Helmet } from "react-helmet"
 import { graphql } from "gatsby"
 import Img from "gatsby-image"
 import { format } from "date-fns"
-import { Fade } from "react-awesome-reveal"
+import { Fade, Slide } from "react-awesome-reveal"
 import "../sass/trustfulrobot.scss"
 import svg_logo from "../../static/images/TrustfulRobot_DigitalShamanism.svg"
 import social_graph_img from "../../static/images/socialgraphimg.jpg"
@@ -225,8 +225,10 @@ function HomePage({ data }) {
       </div>
       <main>
         <section className="tr_block tr_block__intro">
-          <Fade duration={3000}>
+          <Fade triggerOnce duration={1500} >
             <h1><img src={`${svg_logo}`} alt="Trustful Robot | Digital Shamanism" /></h1>
+          </Fade>
+          <Fade duration={3000} triggerOnce >
             <div id="introtext">
               <p>I am a Senior Developer with a 20-year history of providing services across the full stack who has also at times served as a content author as well as a liaison with website clients. As such, I feel that I have a <em>deep sensitivity</em> to the entire spectrum of perspectives of those involved in the complete life-cycle of a website.</p>
 
@@ -238,137 +240,155 @@ function HomePage({ data }) {
         </section>
 
         <section className="tr_block tr_block__technology">
-          <Fade duration={2000}>
+          <Fade duration={3000} triggerOnce >
             <h2>Technology</h2>
             <div id="techstacks">
-              <section>
-                <h3>My preferred <strong>WordPress</strong> tech stack:</h3>
-                <ul>
-                  <li><strong>Underscores</strong> starter theme</li>
-                  <li><strong>WPGulp</strong> workflow based on <strong>npm</strong> and <strong>SASS</strong></li>
-                  <li><strong>Advanced Custom Fields Pro</strong></li>
-                </ul>
-              </section>
-              <section>
-                <h3>My "cutting-edge" tech stack:</h3>
-                <ul>
-                  <li>Static site generation with <strong>GatsbyJS</strong> and <strong>GraphQL</strong></li>
-                  <li>Content management with <strong>Prismic</strong></li>
-                  <li>Hosting with <strong>Netlify</strong> & <strong>AWS</strong></li>
-                  <li><strong>Continuous Integration, Deployment, and Delivery</strong> with <strong>GitHub</strong></li>
-                </ul>
-              </section>
+              <Fade duration={1500} delay={500} triggerOnce cascade damping={0.25} >
+                <section>
+                  <h3>My preferred <strong>WordPress</strong> tech stack:</h3>
+                  <ul>
+                    <li><strong>Underscores</strong> starter theme</li>
+                    <li><strong>WPGulp</strong> workflow based on <strong>npm</strong> and <strong>SASS</strong></li>
+                    <li><strong>Advanced Custom Fields Pro</strong></li>
+                  </ul>
+                </section>
+                <section>
+                  <h3>My "cutting-edge" tech stack:</h3>
+                  <ul>
+                    <li>Static site generation with <strong>GatsbyJS</strong> and <strong>GraphQL</strong></li>
+                    <li>Content management with <strong>Prismic</strong></li>
+                    <li>Hosting with <strong>Netlify</strong> & <strong>AWS</strong></li>
+                    <li><strong>Continuous Integration, Deployment, and Delivery</strong> with <strong>GitHub</strong></li>
+                  </ul>
+                </section>
+              </Fade>
             </div>
           </Fade>
         </section>
         <section className="tr_block tr_block__work">
-          <h2>Work</h2>
+          <Fade duration={3000}>
+            <h2>Work</h2>
+          </Fade>
           <div id="worklist">
           <ul>
-          <li>
-            <section>
-              <div className="workimage__wrapper">
-                <a href="https://castleandkey.com/" target="_blank" rel="noopener noreferrer">
-                  <Img 
-                    fluid={castleandkey_sources} 
-                    alt="Castle & Key Distillery Website"
-                  />
-                </a>
-              </div>
-              <div className="workdetails__wrapper">
-                <div className="workdetails">
-                  <h4>Castle & Key Distillery</h4>
-                  <p>Fully-custom <strong>Shopify</strong> development</p>
-                  <div className="linkwrapper"><a href="https://castleandkey.com/" target="_blank" rel="noopener noreferrer">castleandkey.com</a></div>
-                </div>
-              </div>
-            </section>
-          </li>
-          <li>
-            <section>
-              <div className="workimage__wrapper">
-                <a href="https://en.lbxco.com/" target="_blank" rel="noopener noreferrer">
-                  <Img 
-                    fluid={lbx_sources}
-                    alt="Link-Belt Excavators Website" 
-                  />
-                </a>
-              </div>
-              <div className="workdetails__wrapper">
-                <div className="workdetails">
-                  <h4>Link-Belt Excavators</h4>
-                  <p>Fully-custom <strong>WordPress</strong> development with personalized content builder</p>
-                  <div className="linkwrapper"><a href="https://en.lbxco.com/" target="_blank" rel="noopener noreferrer">en.lbxco.com</a></div>
-                </div>
-              </div>
-            </section>
-          </li>
-          <li>
-            <section>
-              <div className="workimage__wrapper">
-                <a href="https://bullhorncreative.com/" target="_blank" rel="noopener noreferrer">
-                  <Img 
-                    fluid={bullhorn_sources}
-                    alt="Bullhorn Creative Website" 
-                  />
-                </a>
-              </div>
-              <div className="workdetails__wrapper">
-                <div className="workdetails">
-                  <h4>Bullhorn Creative</h4>
-                  <p>Fully-custom <strong>WordPress</strong> development with personalized content builder</p>
-                  <div className="linkwrapper"><a href="https://bullhorncreative.com/" target="_blank" rel="noopener noreferrer">bullhorncreative.com</a></div>
-                </div>
-              </div>
-            </section>
-          </li>
-          <li>
-            <section>
-              <div className="workimage__wrapper">
-                <a href="https://granulata.com/" target="_blank" rel="noopener noreferrer">
-                  <Img 
-                    fluid={granulata_sources}
-                    alt="Granulata Website" 
-                  />
-                </a>
-              </div>
-              <div className="workdetails__wrapper">
-                <div className="workdetails">
-                  <h4>Granulata</h4>
-                  <p>Website to accompany exhibit at <strong>The 2020 Music, Art, and Technology Fair</strong> at Georgia Tech</p>
-                  <div className="linkwrapper"><a href="https://granulata.com/" target="_blank" rel="noopener noreferrer">granulata.com</a></div>
-                </div>
-              </div>
-            </section>
-          </li>
-          <li>
-            <section>
-              <div className="workimage__wrapper">
-                <a href="http://patrickmorrissey.com/" target="_blank" rel="noopener noreferrer">
-                  <Img 
-                    fluid={patrickmorrissey_sources}
-                    alt="Patrick Morrissey Website" 
-                  />
-                </a>
-              </div>
-              <div className="workdetails__wrapper">
-                <div className="workdetails">
-                  <h4>Patrick Morrissey</h4>
-                  <p>Semantic Self-Portrait</p>
-                  <div className="linkwrapper"><a href="http://patrickmorrissey.com/" target="_blank" rel="noopener noreferrer">patrickmorrissey.com</a></div>
-                </div>
-              </div>
-            </section>
-          </li>
-          </ul>
+            <Fade triggerOnce duration={3000} direction={"up"}>
+              <li>
+                <section>
+                  <div className="workimage__wrapper">
+                    <a href="https://castleandkey.com/" target="_blank" rel="noopener noreferrer">
+                      <Img 
+                        fluid={castleandkey_sources} 
+                        alt="Castle & Key Distillery Website"
+                      />
+                    </a>
+                  </div>
+                  <div className="workdetails__wrapper">
+                    <div className="workdetails">
+                      <h4>Castle & Key Distillery</h4>
+                      <p>Fully-custom <strong>Shopify</strong> development</p>
+                      <div className="linkwrapper"><a href="https://castleandkey.com/" target="_blank" rel="noopener noreferrer">castleandkey.com</a></div>
+                    </div>
+                  </div>
+                </section>
+              </li>
+            </Fade>
+            <Fade triggerOnce duration={3000} direction={"up"}>
+              <li>
+                <section>
+                  <div className="workimage__wrapper">
+                    <a href="https://en.lbxco.com/" target="_blank" rel="noopener noreferrer">
+                      <Img 
+                        fluid={lbx_sources}
+                        alt="Link-Belt Excavators Website" 
+                      />
+                    </a>
+                  </div>
+                  <div className="workdetails__wrapper">
+                    <div className="workdetails">
+                      <h4>Link-Belt Excavators</h4>
+                      <p>Fully-custom <strong>WordPress</strong> development with personalized content builder</p>
+                      <div className="linkwrapper"><a href="https://en.lbxco.com/" target="_blank" rel="noopener noreferrer">en.lbxco.com</a></div>
+                    </div>
+                  </div>
+                </section>
+              </li>
+            </Fade>
+            <Fade triggerOnce duration={3000} direction={"up"}>
+              <li>
+                  <section>
+                    <div className="workimage__wrapper">
+                      <a href="https://bullhorncreative.com/" target="_blank" rel="noopener noreferrer">
+                        <Img 
+                          fluid={bullhorn_sources}
+                          alt="Bullhorn Creative Website" 
+                        />
+                      </a>
+                    </div>
+                    <div className="workdetails__wrapper">
+                      <div className="workdetails">
+                        <h4>Bullhorn Creative</h4>
+                        <p>Fully-custom <strong>WordPress</strong> development with personalized content builder</p>
+                        <div className="linkwrapper"><a href="https://bullhorncreative.com/" target="_blank" rel="noopener noreferrer">bullhorncreative.com</a></div>
+                      </div>
+                    </div>
+                  </section>
+              </li>
+            </Fade>
+            <Fade triggerOnce duration={3000} direction={"up"}>
+              <li>
+                  <section>
+                    <div className="workimage__wrapper">
+                      <a href="https://granulata.com/" target="_blank" rel="noopener noreferrer">
+                        <Img 
+                          fluid={granulata_sources}
+                          alt="Granulata Website" 
+                        />
+                      </a>
+                    </div>
+                    <div className="workdetails__wrapper">
+                      <div className="workdetails">
+                        <h4>Granulata</h4>
+                        <p>Website to accompany exhibit at <strong>The 2020 Music, Art, and Technology Fair</strong> at Georgia Tech</p>
+                        <div className="linkwrapper"><a href="https://granulata.com/" target="_blank" rel="noopener noreferrer">granulata.com</a></div>
+                      </div>
+                    </div>
+                  </section>
+              </li>
+            </Fade>
+            <Fade triggerOnce duration={3000} direction={"up"}>
+              <li>
+                  <section>
+                    <div className="workimage__wrapper">
+                      <a href="http://patrickmorrissey.com/" target="_blank" rel="noopener noreferrer">
+                        <Img 
+                          fluid={patrickmorrissey_sources}
+                          alt="Patrick Morrissey Website" 
+                        />
+                      </a>
+                    </div>
+                    <div className="workdetails__wrapper">
+                      <div className="workdetails">
+                        <h4>Patrick Morrissey</h4>
+                        <p>Semantic Self-Portrait</p>
+                        <div className="linkwrapper"><a href="http://patrickmorrissey.com/" target="_blank" rel="noopener noreferrer">patrickmorrissey.com</a></div>
+                      </div>
+                    </div>
+                  </section>
+              </li>
+            </Fade>
+            </ul>
           </div>
         </section>
         <section className="tr_block tr_block__instagram">
-          <h2>Instagram</h2>
+          <Fade duration={3000} triggerOnce >
+            <h2>Instagram</h2>
+          </Fade>
           <div id="instagramlist">
-            <ul>
-              {allInstagramDOM}
-            </ul>
+            <Fade triggerOnce duration={3000} >
+              <ul>
+                {allInstagramDOM}
+              </ul>
+            </Fade>
             <p className="ig-account-link">See all: <a href="https://www.instagram.com/frightlab/" target="_blank" rel="noopener noreferrer">instagram.com/frightlab</a></p>
           </div>
         </section>
@@ -394,23 +414,26 @@ function HomePage({ data }) {
         </section>
 
         <section id="originstory" className="tr_block tr_block__origin">
-          <h2>Origin Story</h2>
-          <div>
-            <h3>So what exactly does <strong>Trustful Robot</strong> mean and from where did the name come?</h3>
+          <Fade duration={3000} triggerOnce >
+            <h2>Origin Story</h2>
 
-            <p>For the meaning, I will smile wryly and leave you to decide for yourself what it could mean but, as for the name's conception, it emerged from an agency talk that I was leading on computational creativity.  I spoke of what actually constitutes "creativity" from a computer or scientist's perspective and provided examples of specific methods and procedures that allow one to collaborate with Chance.  As a final group exercise, we all created our own fold-in prose experiments (instructions below) in order to generate a new WiFi password for the office.  "Trustful Robot" was the phrase that we collectively manifested and I began to really like it as an abstract idea.</p>
+            <div>
+              <h3>So what exactly does <strong>Trustful Robot</strong> mean and from where did the name come?</h3>
 
-            <h4>You can view the slides that accompanied my talk in this PDF:</h4>
-            <p><a href="/the_unseen_collaborator.pdf">the_unseen_collaborator.pdf</a></p>
+              <p>For the meaning, I will smile wryly and leave you to decide for yourself what it could mean but, as for the name's conception, it emerged from an agency talk that I was leading on computational creativity.  I spoke of what actually constitutes "creativity" from a computer or scientist's perspective and provided examples of specific methods and procedures that allow one to collaborate with Chance.  As a final group exercise, we all created our own fold-in prose experiments (instructions below) in order to generate a new WiFi password for the office.  "Trustful Robot" was the phrase that we collectively manifested and I began to really like it as an abstract idea.</p>
 
-            <h4>Instructions for generating your own cut-up/fold-in experiments:</h4>
-            <ol>
-              <li>Choose 2-3 text sources (anything written - novels, technical manuals, journal entries, news articles, advertisements, total gibberish, etc).  I enjoy trying to make these as wildly different from one another as possible but you're the mastermind here - play by your own rules.</li>
-              <li>Take a single piece of paper and fold it into as many equal columns as you have text sources.  Lined paper will help but isn't required.</li>
-              <li>Find sections from each text source that you would like to "sample" and then, one source per page column, begin transcribing each source passage onto your folded paper by hand.  The more source text you have for each column, the more raw material you are adding in to the machine to be processed.</li>
-              <li>Unfold your paper after you have finished transcribing your single source texts and then read across the page as you would normally.  You will <em>mostly</em> get a bunch of garbage, garbled nonsense out but, if you're willing to sift through the results, I've found that I almost always generate <em>something</em> interesting and curious and new with this process.  Be open-minded to delight and wonder.</li>
-            </ol>
-          </div>
+              <h4>You can view the slides that accompanied my talk in this PDF:</h4>
+              <p><a href="/the_unseen_collaborator.pdf">the_unseen_collaborator.pdf</a></p>
+
+              <h4>Instructions for generating your own cut-up/fold-in experiments:</h4>
+              <ol>
+                <li>Choose 2-3 text sources (anything written - novels, technical manuals, journal entries, news articles, advertisements, total gibberish, etc).  I enjoy trying to make these as wildly different from one another as possible but you're the mastermind here - play by your own rules.</li>
+                <li>Take a single piece of paper and fold it into as many equal columns as you have text sources.  Lined paper will help but isn't required.</li>
+                <li>Find sections from each text source that you would like to "sample" and then, one source per page column, begin transcribing each source passage onto your folded paper by hand.  The more source text you have for each column, the more raw material you are adding in to the machine to be processed.</li>
+                <li>Unfold your paper after you have finished transcribing your single source texts and then read across the page as you would normally.  You will <em>mostly</em> get a bunch of garbage, garbled nonsense out but, if you're willing to sift through the results, I've found that I almost always generate <em>something</em> interesting and curious and new with this process.  Be open-minded to delight and wonder.</li>
+              </ol>
+            </div>
+          </Fade>
         </section>
 
       </main>
